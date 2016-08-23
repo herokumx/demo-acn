7027e4fcf668bca79d3c7b80849fe1871ac12671
baliles <david@baliles.com>
2016-08-23 10:03:08 -0400

    Changed to IOTSensor Topic

--- src/main/scala/SalesforceToKafka.scala
+++ src/main/scala/SalesforceToKafka.scala
@@ -11,13 +11,13 @@ object SalesforceToKafka extends App {
 
   implicit val actorSystem = ActorSystem()
 
-  Salesforce.withSource("IOTCaseUpdates") { salesforceSource =>
-    Kafka.sink[String]("IOTCaseUpdates").map { kafkaSink =>
+  Salesforce.withSource("IOTSensor") { salesforceSource =>
+    Kafka.sink[String]("IOTSensor").map { kafkaSink =>
       implicit val materializer = ActorMaterializer()(actorSystem)
 
       def messageToProducerRecord(message: Message) = {
         logger.info("Got message: " + message.getJSON)
-        new ProducerRecord[String, String]("IOTCaseUpdates", message.getJSON)
+        new ProducerRecord[String, String]("IOTSensor", message.getJSON)
       }
 
       salesforceSource.map(messageToProducerRecord).to(kafkaSink).run()
